b5e476687551eb59158cbda1e96240ad83b7ab1f
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2019-11-05 14:32:16 +0300

    update

--- run.sh
+++ run.sh
@@ -3,4 +3,36 @@
 # Script for first run after installed CentOS servers
 # Install necessary tools, remove unused software
 
-yum erase iwl* -y
+# Sys env / paths / etc
+# -------------------------------------------------------------------------------------------\
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+
+# Firs steps
+# -------------------------------------------------------------------------------------------\
+
+# Remove unused software
+yum erase iwl* -н
+
+# Update system
+yum update -y 
+
+# Install software
+yum install git nano wget net-tools epel-release -y
+
+
+read -p "Install Rmate? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    wget -O install-rmate.sh https://raw.githubusercontent.com/m0zgen/install-rmate/master/install-rmate.sh && bash install-rmate.sh
+    rm -f install-rmate.sh
+fi
+
+read -p "Secure SSH? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    wget -O secure-ssh.sh https://raw.githubusercontent.com/m0zgen/secure-ssh/master/secure-ssh.sh && bash secure-ssh.sh
+    rm -f secure-ssh.sh
+fi
+
+exit 0
