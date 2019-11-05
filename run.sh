f3d1175cd742764cccc35b05ad51dc067225d60e
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2019-11-05 14:47:18 +0300

    fix

--- run.sh
+++ run.sh
@@ -29,7 +29,7 @@ install_rmate()
 	if echo "$answer" | grep -iq "^y" ;then
 		wget -O install-rmate.sh https://raw.githubusercontent.com/m0zgen/install-rmate/master/install-rmate.sh && bash install-rmate.sh
 	    rm -f install-rmate.sh
-	else
+	fi
 }
 
 if [ -f "$RMATE" ]; then
@@ -44,7 +44,7 @@ read answer
 if echo "$answer" | grep -iq "^y" ;then
   wget -O secure-ssh.sh https://raw.githubusercontent.com/m0zgen/secure-ssh/master/secure-ssh.sh && bash secure-ssh.sh
     rm -f secure-ssh.sh
-else
+fi
 
 
 exit 0
