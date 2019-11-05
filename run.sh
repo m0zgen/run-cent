91cb9b449e3a87a1f638e5a6340de9d785587803
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2019-11-05 14:43:10 +0300

    update questions

--- run.sh
+++ run.sh
@@ -20,19 +20,30 @@ yum install git nano wget net-tools epel-release -y
 # Remove unused software
 yum erase iwl* -y
 
+RMATE="/usr/local/bin/rmate"
 
-read -p "Install Rmate? " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    wget -O install-rmate.sh https://raw.githubusercontent.com/m0zgen/install-rmate/master/install-rmate.sh && bash install-rmate.sh
-    rm -f install-rmate.sh
+function install_rmate {
+	echo -en "Install Rmate(y/n)? "
+	read answer
+	if echo "$answer" | grep -iq "^y" ;then
+		wget -O install-rmate.sh https://raw.githubusercontent.com/m0zgen/install-rmate/master/install-rmate.sh && bash install-rmate.sh
+	    rm -f install-rmate.sh
+	else
+}
+
+if [ -f "$RMATE" ]; then
+    echo "$RMATE already installed"
+else
+	install_rmate
 fi
 
-read -p "Secure SSH? " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    wget -O secure-ssh.sh https://raw.githubusercontent.com/m0zgen/secure-ssh/master/secure-ssh.sh && bash secure-ssh.sh
+
+echo -en "Secure SSH?(y/n)? "
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+  wget -O secure-ssh.sh https://raw.githubusercontent.com/m0zgen/secure-ssh/master/secure-ssh.sh && bash secure-ssh.sh
     rm -f secure-ssh.sh
-fi
+else
+
 
 exit 0
